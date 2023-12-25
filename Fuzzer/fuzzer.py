3b23ac19b7cd72b4c8424b6e4d5806924c4462c3
1twodrei <118087221+1twodrei@users.noreply.github.com>
2023-12-25 22:10:47 +0100

    rmv

--- Fuzzer/fuzzer.py
+++ Fuzzer/fuzzer.py
@@ -1,22 +1,17 @@
 from selenium import webdriver
 from urllib.parse import quote
 
-# Load payload parameters from the file
 with open('/payload-parameters.txt', 'r') as file:
     payload_parameters = [line.strip() for line in file.readlines() if line.strip()]
 
-# Assuming you have a vulnerable URL to test
+
 vulnerable_url = 'https://example.com/vulnerable_endpoint'
 
-# Initialize the Chrome WebDriver (you can use other browsers too)
-driver = webdriver.Chrome()
+driver = webdriver.Safari()
 
-# Loop through each payload parameter
 for parameter in payload_parameters:
-    # Construct the URL with the parameter to test
     url_with_payload = f"{vulnerable_url}?input={quote(parameter)}"
 
-    # Open the URL
     driver.get(url_with_payload)
 
     # Check if an alert is present (indicating potential XSS)
@@ -25,7 +20,6 @@ for parameter in payload_parameters:
         print(f"XSS Possible with parameter: {parameter}")
         alert.dismiss()  # Dismiss the alert
     except:
-        pass  # No alert found
+        pass  # No alert
 
-# Close the browser session
 driver.quit()
--- Receiver/receiver.php
+++ Receiver/receiver.php
@@ -2,15 +2,12 @@
 $payload = $_GET['xss'] ?? '';
 
 if (!empty($payload)) {
-    // Store the payload in a file
-    $file = fopen('confirmed.txt', 'a'); // Opens the file in append mode
-    fwrite($file, $payload . PHP_EOL); // Write the payload to the file with a new line
+    $file = fopen('confirmed.txt', 'a'); 
+    fwrite($file, $payload . PHP_EOL);
     fclose($file);
 
-    // Optional: Notify the user that the payload was received
     echo 'Received and stored XSS Payload: ' . $payload;
 } else {
-    // No payload received
     echo 'No XSS Payload received';
 }
 ?>
